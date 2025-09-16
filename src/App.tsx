52d1cf115a7d767d417e7aea8d358c341162b44a
v2lentina <vali_190202@hotmail.de>
2025-09-16 09:22:01 +0200

    country detail page setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@
 // Defines the main structure and behavior of the app, including routes and UI components.
 
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
+import CountryDetail from "./pages/CountryDetail";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/country/:name" element={<CountryDetail />} />
     </Routes>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,5 @@ createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <App />
     </StrictMode>
-    ,
   </BrowserRouter>
 );
