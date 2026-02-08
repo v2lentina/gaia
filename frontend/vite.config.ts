480e3990e9989bfa57462b2f4d47cc353a61d810
v2lentina <vali_190202@hotmail.de>
2026-02-08 12:48:41 +0100

    updated weatherapi deployed url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,22 +3,30 @@ import react from "@vitejs/plugin-react-swc";
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/gaia/",
-  plugins: [
-    react(),
-    federation({
-      name: "gaiaHost",
-      remotes: {
-        weatherRemote: "http://localhost:3001/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  const weatherRemoteUrl = isDev
+    ? "http://localhost:3001/assets/remoteEntry.js"
+    : "https://v2lentina.github.io/WeatherApi/assets/remoteEntry.js";
+
+  return {
+    base: "/gaia/",
+    plugins: [
+      react(),
+      federation({
+        name: "gaiaHost",
+        remotes: {
+          weatherRemote: weatherRemoteUrl,
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
